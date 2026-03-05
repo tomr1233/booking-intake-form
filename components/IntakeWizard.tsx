369733bdc822ea98113c93b0cb52ea97449dd040
tomr1233 <tomr@expresstxtapp.com>
2026-03-06 12:48:44 +1300

    add cicd workflow

--- components/IntakeWizard.tsx
+++ components/IntakeWizard.tsx
@@ -36,7 +36,7 @@ const FORM_STEPS = [
     label: 'Numbers',
     subtitle: 'Your metrics matter',
     title: 'Helpful Details',
-    description: 'Helpful details to get started',
+    description: 'Helpful details to get started quickly',
   },
   {
     id: 'vision',
